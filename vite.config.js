32aba274661255e390bd243478a5d75d2fbc5738
63nitin <yadavnitin638675@bbdnitm.ac.in>
2024-04-28 13:29:17 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:"/chai-aur-react/",
+  base:"/portfolio2/",
   plugins: [react()],
 })
